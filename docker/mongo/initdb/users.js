a671f34c4055b63312fc1de9e1361566333b17db
Juan M. Estévez Ferro <jeferro@gmail.com>
2024-05-03 23:29:47 +0200

    add bcrypt

--- docker/mongo/initdb/users.js
+++ docker/mongo/initdb/users.js
@@ -3,7 +3,7 @@ let now = new Date()
 db['users'].insertMany([
     {
         _id: "user",
-        encodedPassword: "user",
+        encodedPassword: "$2a$10$kldEms9W7KeY8Kf1We92E.L8L1ywr7dGGe2fuy49qraomTy4T15Ya",
         roles: [
             'user'
         ],
