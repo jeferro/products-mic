eb953cb96fef2016a811361dc9b91fb72c4aec9b
Juan M. Estévez Ferro <jeferro@gmail.com>
2024-11-10 22:27:15 +0100

    Fix avro compilation

--- code/buildSrc/src/main/java/com/jeferro/plugins/avro_generator/AvroGeneratorTask.java
+++ code/buildSrc/src/main/java/com/jeferro/plugins/avro_generator/AvroGeneratorTask.java
@@ -5,9 +5,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.avro.Schema;
 import org.apache.avro.compiler.specific.SpecificCompiler;
-import org.apache.avro.generic.GenericData.StringType;
 import org.gradle.api.Action;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
@@ -15,34 +13,34 @@ import org.gradle.api.tasks.compile.JavaCompile;
 
 public class AvroGeneratorTask implements Action<Task> {
 
-	private static final String TASK_ID = "avroGeneratorTask";
+  private static final String TASK_ID = "avroGeneratorTask";
 
-	private final File schemaDir;
+  private final File schemaDir;
 
-	private final File targetDir;
+  private final File targetDir;
 
-	public AvroGeneratorTask(File schemaDir, File targetDir) {
-		this.schemaDir = schemaDir;
-		this.targetDir = new File(targetDir, "src/main/java");
-	}
+  public AvroGeneratorTask(File schemaDir, File targetDir) {
+	this.schemaDir = schemaDir;
+	this.targetDir = new File(targetDir, "src/main/java");
+  }
 
-	public static void register(Project project, File schemasDir, File targetDir) {
-		AvroGeneratorTask avroGeneratorTask = new AvroGeneratorTask(schemasDir, targetDir);
-		project.task(TASK_ID, avroGeneratorTask);
+  public static void register(Project project, File schemasDir, File targetDir) {
+	AvroGeneratorTask avroGeneratorTask = new AvroGeneratorTask(schemasDir, targetDir);
+	project.task(TASK_ID, avroGeneratorTask);
 
-		project.getTasks().withType(JavaCompile.class)
-			.configureEach(javaCompile -> javaCompile.dependsOn(TASK_ID));
-	}
+	project.getTasks().withType(JavaCompile.class)
+		.configureEach(javaCompile -> javaCompile.dependsOn(TASK_ID));
+  }
 
-	@Override
-	public void execute(Task task) {
-		task.doLast(this::doLast);
-	}
+  @Override
+  public void execute(Task task) {
+	task.doLast(this::doLast);
+  }
 
-	private void doLast(Task task) {
-		try {
-			var avroFiles = getAvroFiles(schemaDir);
-		  SpecificCompiler.compileSchema(avroFiles, targetDir);
+  private void doLast(Task task) {
+	try {
+	  var avroFiles = getAvroFiles(schemaDir);
+	  SpecificCompiler.compileSchema(avroFiles, targetDir);
 		  /*Schema.Parser parser = new Schema.Parser();
 
 		  for(var avroFile : avroFiles){
@@ -52,29 +50,31 @@ public class AvroGeneratorTask implements Action<Task> {
 			compiler.setStringType(StringType.String);
 			compiler.compileToDestination(avroFile, targetDir);
 		  }*/
-		} catch (IOException cause) {
-			throw new RuntimeException(cause);
-		}
+	} catch (IOException cause) {
+	  throw new RuntimeException(cause);
+	}
+  }
+
+  private File[] getAvroFiles(File schemaDir) {
+	List<File> avroFiles = getAvroFilesRecursively(schemaDir);
+
+	File[] avroFilesArray = new File[avroFiles.size()];
+	return avroFiles.toArray(avroFilesArray);
+  }
+
+  private List<File> getAvroFilesRecursively(File directory) {
+	List<File> schemaFiles = new ArrayList<>();
+
+	for (File file : directory.listFiles()) {
+	  if (file.isDirectory()) {
+		schemaFiles.addAll(getAvroFilesRecursively(file));
+	  } else if (file.getName().matches("imports.avsc")) {
+		schemaFiles.addFirst(file);
+	  } else {
+		schemaFiles.add(file);
+	  }
 	}
 
-	private File[] getAvroFiles(File schemaDir) {
-		List<File> avroFiles = getAvroFilesRecursively(schemaDir);
-
-		File[] avroFilesArray = new File[avroFiles.size()];
-		return avroFiles.toArray(avroFilesArray);
-	}
-
-	private List<File> getAvroFilesRecursively(File directory) {
-		List<File> schemaFiles = new ArrayList<>();
-
-		for (File file : directory.listFiles()) {
-			if (file.isDirectory()) {
-				schemaFiles.addAll(getAvroFilesRecursively(file));
-			} else {
-				schemaFiles.add(file);
-			}
-		}
-
-		return schemaFiles;
-	}
+	return schemaFiles;
+  }
 }
