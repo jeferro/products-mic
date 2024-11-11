916aa7b1611af5628e4c2401f99a7b0e301028c8
Juan M. Estévez Ferro <jeferro@gmail.com>
2024-11-11 07:35:22 +0100

    Fix avro compilation

--- code/app/src/main/java/com/jeferro/products/shared/infrastructure/config/products/ProductsComponentProperties.java
+++ code/app/src/main/java/com/jeferro/products/shared/infrastructure/config/products/ProductsComponentProperties.java
@@ -1,10 +1,12 @@
 package com.jeferro.products.shared.infrastructure.config.products;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
+@Setter
 @Configuration
 @ConfigurationProperties("components.products")
 public class ProductsComponentProperties {
