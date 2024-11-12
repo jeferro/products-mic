2919c3e165b20a17356ce492f31935c5f17a536c
jeferro <38972129+jeferro@users.noreply.github.com>
2024-11-12 21:31:07 +0100

    Upgrade Spring to 3.3.5 (#18)
    * Fix local data
    
    * Upgrade to Spring Boot 3.3.5

--- code/app/src/test/java/com/jeferro/products/products/parametrics/domain/models/ProductTypeMother.java
+++ code/app/src/test/java/com/jeferro/products/products/parametrics/domain/models/ProductTypeMother.java
@@ -3,7 +3,6 @@ package com.jeferro.products.products.parametrics.domain.models;
 import com.jeferro.products.parametrics.domain.models.values.ParametricValue;
 import com.jeferro.products.parametrics.domain.models.values.ParametricValueId;
 import com.jeferro.shared.locale.domain.models.LocalizedField;
-import org.jetbrains.annotations.NotNull;
 
 public class ProductTypeMother {
 
@@ -14,7 +13,6 @@ public class ProductTypeMother {
 		LocalizedField.createOfUS("Fruit"));
   }
 
-  @NotNull
   public static ParametricValueId fruitId() {
 	return ParametricValueId.createOf("fruit");
   }
